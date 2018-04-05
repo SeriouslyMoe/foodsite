8a839d8e031a4a4add23289b34b8c0970613efb2
Alex Manbavaran <moeshop@outlook.com>
2018-04-05 14:08:59 -0400

    CSS Updates
    Freshened the page up with border radius, text colors, getting rid of the scrollbar bug, font update, and deleting of trash files.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var randomNumber = Math.floor((Math.random() * 17) + 1);
-  var randomBackground = randomNumber + ".jpg";
-  console.log(randomNumber);
+	var randomNumber = Math.floor((Math.random() * 17) + 1);
+	var randomBackground = randomNumber + ".jpg";
+	console.log(randomNumber);
 
-  document.querySelector("div#picture").style.backgroundImage = "url(assets/a/" + randomBackground + ")";
-  document.querySelector("div.background").style.backgroundImage = "url(assets/bg/" + randomBackground + ")";
+	document.querySelector("div#picture").style.backgroundImage = "url(assets/a/" + randomBackground + ")";
+	document.querySelector("div.background").style.backgroundImage = "url(assets/bg/" + randomBackground + ")";
 });
